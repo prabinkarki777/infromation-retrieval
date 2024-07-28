bedeffbbb91fb457cc43df947ab83859b265dd25
Prabin <78411023+prabin-karki@users.noreply.github.com>
2024-07-28 13:27:59 +0545

    added headers, download delay in settings.py for polite crawling

--- search_engine/search_engine/settings.py
+++ search_engine/search_engine/settings.py
@@ -19,6 +19,14 @@ NEWSPIDER_MODULE = "search_engine.spiders"
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+DEFAULT_REQUEST_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+DOWNLOAD_DELAY = 2
+
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
 
